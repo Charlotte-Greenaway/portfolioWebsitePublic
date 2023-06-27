f319692c1aca1851729ee5369c2a8d3db431717a
charlottegreenaway <charlottegreenaway654@gmail.com>
2023-06-27 02:26:40 +0100

    adding 404 page

--- client/src/Routes/404.js
+++ client/src/Routes/404.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const fourofour=({ScreenWidth})=>{
     return (
-        <main  className={ScreenWidth === 'active' ? 'half' : 'full'}>
+        <main  className={ScreenWidth === 'active' ? 'half' : 'full'} style={{textAlign:"center", fontFamily:"monospace"}}>
             <h1>404 Page Not Found</h1>
+            <h2>Lost?</h2>
+            <h3>Theres no place like <Link to="/">home.</Link></h3>
         </main>
     )
 }
